5a996c732e891db9f9ce5f79c60a1d4574af8b2c
ET-TOUNANI <tounaniabderrahmane99@gmail.com>
2022-03-22 20:40:02 +0100

    add ex5

---
+++ ex5.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+void test(int n=0,float x=2.5){
+    cout <<"Fontion N°1 : ";
+    cout << "n= "<<n<<" x= "<<x<<"\n";
+}
+
+void test(float n=4.1,int x=2){
+    cout <<"Fontion N°2 : ";
+    cout << "n= "<<n<<" x= "<<x<<"\n";
+}
+
+
+int main()
+{
+    int i=5;float r=3.2;
+    test(i,r);//Fontion N°1
+    test(r,i);//Fontion N°1
+    test(i);//Fontion N°1
+    test(r);//Fontion N°1
+
+
+    return 0;
+}
