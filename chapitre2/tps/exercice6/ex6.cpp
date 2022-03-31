7890c7286641e3e477f3e13ef9918baab1540df1
ET-TOUNANI <tounaniabderrahmane99@gmail.com>
2022-03-31 22:25:46 +0000

    add ex 6

---
+++ chapitre2/tps/exercice6/ex6.cpp
@@ -0,0 +1,32 @@
+// exercice 6
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+class SuitAr
+{
+
+    int nbval, *val;
+
+public:
+    SuitAr(int, int);
+    ~SuitAr();
+    void afficher();
+};
+
+SuitAr::SuitAr(int nb, int mul)
+{
+    int i;
+    nbval = nb;
+    val = new int[nbval];
+}
+SuitAr::~SuitAr()
+{
+    cout << "detruire object"<<endl;
+}
+int main()
+{
+    SuitAr ar(2, 4);
+
+    return 0;
+}
