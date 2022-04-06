b1b4b3b6ee27dbe1bd182c7dbbcac144348c1f53
ET-TOUNANI <tounaniabderrahmane99@gmail.com>
2022-04-06 15:23:06 +0000

    add exemple 8

---
+++ chapitre3/exemples/ex8/ex8.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+// La valeur de retour d'une fonction est un objet
+// Transmission par reference
+class point
+{
+    int x, y;
+
+public:
+    point(int abs = 0, int ord = 0)
+    {
+        x = abs;
+        y = ord;
+    } // constructeur
+
+    point &symetrique();
+    void affiche();
+};
+point &point ::symetrique()
+{
+    static point res; // static est obligatoir
+    res.x = -x;
+    res.y = -y;
+    return res;
+}
+
+void point ::affiche()
+{
+    cout << "Le point est en " << x << " et "
+         << " " << y << "\n";
+}
+void main()
+{
+    point a, b(1, 6);
+    a = b.symetrique();
+    a.affiche();
+    b.affiche();
+}
